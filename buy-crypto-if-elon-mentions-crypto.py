a17c048490c4b9b53154906f1119c869a0d46195
Andrei <andreibadoiu@ymail.com>
2021-03-21 14:56:03 +0000

    First Commit

---
+++ buy-crypto-if-elon-mentions-crypto.py
@@ -0,0 +1,143 @@
+#Twitter Scraper module
+import tweepy
+from tweepy import OAuthHandler
+
+#dates module
+from datetime import datetime, date
+from itertools import count
+import time
+import re
+
+#trading terminal
+import MetaTrader5 as mt5
+
+
+# Store Twitter credentials from dev account
+consumer_key = "BXMvxigHr1xX2tAGZfD9N8X99"
+consumer_secret = "J5tKhUUY6Yw8bALcEtHa5OwFTy6nz6vXHwNf3HZt9PvlI4UyXc"
+access_key = "77040086-eJ1Ol2ZvI9kRv81ffFkdLSF3RRlFkiDNrDRydpI1v"
+access_secret = "e3ttjiSRJJ0BiLeJCytpIWDU3L3q8LuJvwU0cVhdxulWR"
+
+# Pass twitter credentials to tweepy via its OAuthHandler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+
+# connect to the trade account without specifying a password and a server
+mt5.initialize()
+
+# account number in the top left corner of the MT5 terminal window
+# the terminal database password is applied if connection data is set to be remembered
+account_number = 2016814
+authorized = mt5.login(account_number)
+
+if authorized:
+    print(f'connected to account #{account_number}')
+else:
+    print(f'failed to connect at account #{account_number}, error code: {mt5.last_error()}')
+
+# store the equity of your account
+account_info = mt5.account_info()
+if account_info is None:
+    raise RuntimeError('Could not load the account equity level.')
+else:
+    equity = float(account_info[10])
+
+#crypto sign and keywords
+CRYPTO='BTCUSD'
+keywords =['Bitcoin', 'bitcoin', 'BITCOIN', 'btc', 'BTC']
+
+
+#Get Technoking's latest tweet
+def get_elons_tweet():
+    """Get Elon's last tweet by user ID"""
+    tweets = tweepy.Cursor(api.user_timeline,id="44196397", since=date.today(), tweet_mode='extended').items(1)
+
+    #remove all invalid characters
+    elons_last_tweet = [re.sub('[^A-Za-z0-9]+', ' ', tweet.full_text) for tweet in tweets]
+
+    #re-try until it returns a value - tweepy API fails to return the tweet sometimes
+    while not elons_last_tweet:
+        tweets = tweepy.Cursor(api.user_timeline,id="44196397", since=date.today(), tweet_mode='extended').items(1)
+        elons_last_tweet = [re.sub('[^A-Za-z0-9]+', ' ', tweet.full_text) for tweet in tweets]
+    return elons_last_tweet[0]
+
+
+#buy bitcoin
+def trade():
+    """Check if Musk mentioned bitcoin and open a buy position if so"""
+    what_musk_said = get_elons_tweet()
+
+    # used to check if a position has already been placed
+    positions = mt5.positions_get(symbol=CRYPTO)
+    orders = mt5.orders_get(symbol=CRYPTO)
+    symbol_info = mt5.symbol_info(CRYPTO)
+    price = mt5.symbol_info_tick(CRYPTO).bid
+
+    # perform logic check
+    if any(keyword in what_musk_said for keyword in keywords):
+        print(f'the madlad said it - buying some!')
+
+        # prepare the trade request
+        if not mt5.initialize():
+            raise RuntimeError(f'MT5 initialize() failed with error code {mt5.last_error()}')
+
+        # check that there are no open positions or orders
+        if len(positions) == 0 and len(orders) < 1:
+            if symbol_info is None:
+                print(f'{CRYPTO} not found, can not call order_check()')
+                mt5.shutdown()
+
+            # if the symbol is unavailable in MarketWatch, add it
+            if not symbol_info.visible:
+                print(f'{CRYPTO} is not visible, trying to switch on')
+                if not mt5.symbol_select(CRYPTO, True):
+                    print('symbol_select({}}) failed, exit', CRYPTO)
+
+            #this represents 5% Equity. Minimum order is 0.01 BTC. Increase equity share if retcode = 10014
+            lot = float(round(((equity / 5) / price), 2))
+
+            # define stop loss and take profit
+            sl = price - (price * 5) / 100
+            tp = price + (price * 10) / 100
+            request = {
+                'action': mt5.TRADE_ACTION_DEAL,
+                'symbol': CRYPTO,
+                'volume': lot,
+                'type': mt5.ORDER_TYPE_BUY,
+                'price': price,
+                'sl': sl,
+                'tp': tp,
+                'magic': 66,
+                'comment': 'python-buy',
+                'type_time': mt5.ORDER_TIME_GTC,
+                'type_filling': mt5.ORDER_FILLING_IOC,
+            }
+
+            # send a trading request
+            result = mt5.order_send(request)
+
+            # check the execution result
+            print(f'1. order_send(): by {CRYPTO} {lot} lots at {price}')
+
+            if result.retcode != mt5.TRADE_RETCODE_DONE:
+                print(f'2. order_send failed, retcode={result.retcode}')
+
+            #print the order result - anything else than retcode=10009 is an error in the trading request.
+            print(f'2. order_send done, {result}')
+            print(f'   opened position with POSITION_TICKET={result.order}')
+
+        else:
+            print(f'BUY signal detected, but {CRYPTO} has {len(positions)} active trade')
+
+    else:
+        print(f'He did not say it, he said: {what_musk_said}')
+
+#execute code every 5 seconds
+if __name__ == '__main__':
+    print('Press Ctrl-C / Ctrl-Q to stop.')
+    for i in count():
+        trade()
+        print(f'Iteration {i}')
+        time.sleep(5)
---
+++ readme.txt
@@ -0,0 +1,18 @@
+DESCRIPTION
+
+This bot is designed to buy bitcoin every time Elon musk tweets about bitcoin with the following parameters:
+
+The bot will open a buy position on bitcoin every time Elon mentions bitcoin in his tweet
+Take profit is set to 10% and stop loss to 5%
+The bitcoin bot will not place another trade if there is already an active trade (can be adjusted) 
+
+The bot uses the Tweepy API and a Twitter developer account that you will need access to, as well as a MetaTrader 5 account.
+
+For a detailed guide on how to set up all the resources you need for this project please visit: https://www.cryptomaton.org/2021/03/21/program-a-trading-bot-to-buy-bitcoin-when-musk-tweets-about-it
+
+USE CASES
+
+This particular project is mainly built for fun and the novelty of it. it has not been tested on a live account and there isn't any details analysis to suggest this might work. 
+If you do test this, please share the results!
+
+Want to collaborate? Get in touch.
